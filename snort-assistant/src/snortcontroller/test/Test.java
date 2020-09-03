800f47fe4a4e4310eabc9bea6e18a57880ca34af
Kwonkyu <park2348190@naver.com>
2020-09-02 23:30:40 -0700

    disabled test code. develop first, test later

--- snort-assistant/src/snortcontroller/test/Test.java
+++ snort-assistant/src/snortcontroller/test/Test.java
@@ -7,12 +7,12 @@ public class Test {
     private RuleParser ruleParser;
 
     public Test(){
-        pcapParser = new PcapParser("/home/kwonkyu/Documents/snortlog.pcap");
-        ruleParser = new RuleParser("/home/kwonkyu/Documents/community-icmp.rules");
+        // pcapParser = new PcapParser("/home/kwonkyu/Documents/snortlog.pcap");
+        // ruleParser = new RuleParser("/home/kwonkyu/Documents/community-icmp.rules");
     }
 
     public void test(){
-        pcapParser.parse();
-        ruleParser.parseBody();
+        // pcapParser.parse();
+        // ruleParser.parseBody();
     }
 }
